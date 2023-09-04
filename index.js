0891ee3757df47316cbfe64fd72059893d8bece3
E.dev <fahimermo@gmail.com>
2023-09-04 20:52:14 +0200

    update description and small fix

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ require("module-alias/register");
 
 const app = require("@src/app");
 const logger = require("@src/utils/logger");
+const { exec } = require('child_process');
+
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   logger.info(`API is listening on port ${PORT}`);
+  console.log(`Please open your web browser and visit http://localhost:${PORT}`);
+
+  // Open the browser when the server is running
+  exec(`open http://localhost:${PORT}`);
+ 
 });
